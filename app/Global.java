52ec85ca511f98ef4aaa2f1f5df728d790f7a91b
Sebastien Bro <brosebastien@gmail.com>
2015-08-14 16:39:20 +0200

    Parrot CSS + Initial Load

--- app/Global.java
+++ app/Global.java
@@ -10,16 +10,28 @@ import models.*;
 public class Global extends GlobalSettings {
     
     public void onStart(Application app) {
-        //InitialData.insert(app);
+        InitialData.insert(app);
     }
     
     static class InitialData {
-        /*
+        
         public static void insert(Application app) {
+            Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
             if(Ebean.find(User.class).findRowCount() == 0) {
-                Map<String,List<Object>> all = (Map<String,List<Object>>)Yaml.load("initial-data.yml");
                 Ebean.save(all.get("users"));
-                Ebean.save(all.get("globalOrders"));
+                Logger.info("Users loaded!");
+            }
+            if(Ebean.find(Product.class).findRowCount() == 0) {
+                Ebean.save(all.get("products"));
+                Logger.info("Products loaded!");
+            }
+            // if(Ebean.find(GlobalOrder.class).findRowCount() == 0) {
+            //     Ebean.save(all.get("globalOrders"));
+            //     Logger.info("GlobalOrders loaded!");
+            // }
+            if(Ebean.find(UserOrder.class).findRowCount() == 0) {
+                Ebean.save(all.get("userOrders"));
+                Logger.info("UserOrders loaded!");
             }
                 // // Insert projects
                 // Ebean.save(all.get("projects"));
@@ -31,7 +43,7 @@ public class Global extends GlobalSettings {
                 // // Insert tasks
                 // Ebean.save(all.get("tasks"));
                 
-        }*/
+        }
         
     }
     
